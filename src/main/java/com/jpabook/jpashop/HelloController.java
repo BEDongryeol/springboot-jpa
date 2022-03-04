158ba9990c1050146976234e5b3d9bda18a8b5b9
BEDongryeol <ehdfuf53@gmail.com>
2022-03-04 17:02:06 +0900

    feat: front view configuration

---
+++ src/main/java/com/jpabook/jpashop/HelloController.java
@@ -0,0 +1,16 @@
+package com.jpabook.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        // return되는 String은 페이지 이름이다.
+        return "hello";
+    }
+}
