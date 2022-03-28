ad0e941a2de946f87a69dd9d5fe5771ccb4dedc5
BEDongryeol <ehdfuf53@gmail.com>
2022-03-28 22:12:21 +0900

    feat: Member 정보 조회
    - DTO를 사용해서 확장성 보장

--- src/main/java/com/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/com/jpabook/jpashop/api/MemberApiController.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +17,20 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    }
 
+    @GetMapping("/api/v2/members")
+    public Result memberV2(){
+        List<Member> members = memberService.findMembers();
+        List<MemberDTO> collect = members.stream()
+                .map(m -> new MemberDTO(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(), collect);
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
@@ -40,6 +55,19 @@ public class MemberApiController {
         // query
         Member member = memberService.findOne(id);
         return new UpdateMemberResponse(member.getId(), member.getName());
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDTO {
+        private String name;
     }
 
     @Data
